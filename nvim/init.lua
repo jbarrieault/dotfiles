9e45f1d52c0805bd3f532d6b08e9544204115064
Jacob Barrieault <jacob.barrieault@gmail.com>
2026-01-01 09:48:36 -0500

    Add telescope live_grep mapping

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,6 +5,9 @@ vim.cmd("set shiftwidth=2")
 
 require("config.lazy")
 
-vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, {
-  desc = "Find files"
-})
+local telescope = require("telescope.builtin")
+
+
+vim.keymap.set("n", "<leader>ff", telescope.find_files)
+vim.keymap.set("n", "<leader>fg", telescope.live_grep)
+

de642d71abec75144f5b0ba5793637e338fe3196
Jacob Barrieault <jacob.barrieault@gmail.com>
2026-01-01 09:49:53 -0500

    Add telescope buffers mapping

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,4 +10,5 @@ local telescope = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>ff", telescope.find_files)
 vim.keymap.set("n", "<leader>fg", telescope.live_grep)
+vim.keymap.set("n", "<leader>fb", telescope.buffers)
 

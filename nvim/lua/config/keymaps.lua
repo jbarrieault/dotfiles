1a0d4fcd7a266db43cf0d697c0f7f4f62ec9352b
Jacob Barrieault <jacob.barrieault@gmail.com>
2026-01-02 13:41:07 -0500

    add <leader>[ and <leader>] to change bufferline virtual tabs

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+-- Bufferline navigation
+vim.api.nvim_set_keymap("n", "<leader>]", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>[", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
